a38a3ee64b0491d322a0754b241d8ccf8ec94444
Даниил Юрку <vlad.konovalov070@gmail.com>
2025-01-07 15:08:51 +0200

    Update models.py
    Вся хуйня

--- jdm/cars/models.py
+++ jdm/cars/models.py
@@ -8,6 +8,16 @@ class Car(models.Model):
     image = models.ImageField(upload_to='car_images/')
     available = models.BooleanField(default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cars')
+    marka_car = models.CharField(max_length=100)  # Марка автомобиля
+    color_car = models.CharField(max_length=50)  # Цвет автомобиля
+    country_car = models.CharField(max_length=100)  # Страна производства
+    year_car = models.PositiveIntegerField()  # Год изготовления
+    mileage_car = models.PositiveIntegerField()  # Пробег
+    engine_car = models.CharField(max_length=100)  # Тип двигателя
+    transmission_car = models.CharField(max_length=50)  # Трансмиссия
+    hand_drive = models.CharField(max_length=50)  # Левый/правый руль
+    type_oil = models.CharField(max_length=50)  # Тип масла
+    gear_car = models.CharField(max_length=50)  # Привод
 
     def __str__(self):
         return self.name
